3db2b006f177b5e0dafc65d48a86c57f84c2c2f3
Mikael Boff <mikaelboff1@gmail.com>
2018-07-06 20:32:03 -0300

    Estado e Cidade

---
+++ src/main/java/br/com/mikaelboff/cursospringbootionic/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package br.com.mikaelboff.cursospringbootionic.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import br.com.mikaelboff.cursospringbootionic.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+
+}
