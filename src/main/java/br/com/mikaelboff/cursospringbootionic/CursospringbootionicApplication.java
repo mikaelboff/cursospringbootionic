f280114aa642cd4fc9c4fb76eaa08a4cfbf94b26
Mikael Boff <mikaelboff1@gmail.com>
2018-07-06 00:09:12 -0300

    Criando operação de instanciação

--- src/main/java/br/com/mikaelboff/cursospringbootionic/CursospringbootionicApplication.java
+++ src/main/java/br/com/mikaelboff/cursospringbootionic/CursospringbootionicApplication.java
@@ -1,12 +1,30 @@
 package br.com.mikaelboff.cursospringbootionic;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.mikaelboff.cursospringbootionic.domain.Categoria;
+import br.com.mikaelboff.cursospringbootionic.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursospringbootionicApplication {
+public class CursospringbootionicApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootionicApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 	}
 }
