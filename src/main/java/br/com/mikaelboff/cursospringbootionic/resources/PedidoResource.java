76dc387d35948d2559b4986d1e7022e1e521e2c8
Mikael Boff <mikaelboff1@gmail.com>
2018-07-07 19:34:34 -0300

    Endpoint /pedidos/{id} disponibilizado

--- src/main/java/br/com/mikaelboff/cursospringbootionic/resources/PedidoResource.java
+++ src/main/java/br/com/mikaelboff/cursospringbootionic/resources/PedidoResource.java
@@ -7,19 +7,19 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
-import br.com.mikaelboff.cursospringbootionic.domain.Categoria;
-import br.com.mikaelboff.cursospringbootionic.services.CategoriaService;
+import br.com.mikaelboff.cursospringbootionic.domain.Pedido;
+import br.com.mikaelboff.cursospringbootionic.services.PedidoService;
 
 @RestController
-@RequestMapping(value = "/categorias")
-public class CategoriaResource {
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
 
 	@Autowired
-	private CategoriaService service;
+	private PedidoService service;
 
 	@RequestMapping(value = "/{id}", method = RequestMethod.GET)
 	public ResponseEntity<?> find(@PathVariable Integer id) {
-		Categoria obj = service.buscar(id);
+		Pedido obj = service.buscar(id);
 
 		return ResponseEntity.ok(obj);
 	}
