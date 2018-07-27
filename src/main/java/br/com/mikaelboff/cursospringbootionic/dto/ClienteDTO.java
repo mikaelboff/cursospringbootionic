7ac74931c1db071b8cab2b73647c641bc3b6bb7b
Mikael Boff <mikaelboff1@gmail.com>
2018-07-26 23:36:55 -0300

    Validação customizada: email não repetido na atualização de cliente

--- src/main/java/br/com/mikaelboff/cursospringbootionic/dto/ClienteDTO.java
+++ src/main/java/br/com/mikaelboff/cursospringbootionic/dto/ClienteDTO.java
@@ -8,7 +8,9 @@ import javax.validation.constraints.NotEmpty;
 import org.hibernate.validator.constraints.Length;
 
 import br.com.mikaelboff.cursospringbootionic.domain.Cliente;
+import br.com.mikaelboff.cursospringbootionic.services.validation.ClienteUpdate;
 
+@ClienteUpdate
 public class ClienteDTO implements Serializable {
 	private static final long serialVersionUID = 1L;
 
