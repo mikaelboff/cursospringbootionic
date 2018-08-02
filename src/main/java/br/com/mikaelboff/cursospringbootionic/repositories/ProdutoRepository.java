0ac8ccb01af75457a5818bf4fa6527feab63306d
Mikael Boff <mikaelboff1@gmail.com>
2018-08-01 22:08:39 -0300

    Busca de produtos por nome e categorias

--- src/main/java/br/com/mikaelboff/cursospringbootionic/repositories/ProdutoRepository.java
+++ src/main/java/br/com/mikaelboff/cursospringbootionic/repositories/ProdutoRepository.java
@@ -1,11 +1,26 @@
 package br.com.mikaelboff.cursospringbootionic.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Page;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import br.com.mikaelboff.cursospringbootionic.domain.Categoria;
 import br.com.mikaelboff.cursospringbootionic.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
+
+	// funciona somente com o nome de parâmetro pq é do spring data, sem a jpql
+	// query, e sem o @param
+	@Transactional(readOnly = true)
+	@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome,
+			@Param("categorias") List<Categoria> categorias, Pageable pagerequest);
 
 }
