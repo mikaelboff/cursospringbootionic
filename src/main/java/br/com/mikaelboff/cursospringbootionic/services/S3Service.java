441ba32c86c6ad74c32338904ed841b9ba29e93b
Mikael Boff <mikaelboff1@gmail.com>
2018-09-01 18:25:51 -0300

    Tratando exceções adequadamente

--- src/main/java/br/com/mikaelboff/cursospringbootionic/services/S3Service.java
+++ src/main/java/br/com/mikaelboff/cursospringbootionic/services/S3Service.java
@@ -15,6 +15,8 @@ import org.springframework.web.multipart.MultipartFile;
 import com.amazonaws.services.s3.AmazonS3;
 import com.amazonaws.services.s3.model.ObjectMetadata;
 
+import br.com.mikaelboff.cursospringbootionic.services.exceptions.FileException;
+
 @Service
 public class S3Service {
 
@@ -34,7 +36,7 @@ public class S3Service {
 
 			return uploadFile(is, fileName, contentType);
 		} catch (IOException e) {
-			throw new RuntimeException("Erro de IO ==> " + e.getMessage());
+			throw new FileException("Erro de IO ==> " + e.getMessage());
 		}
 
 	}
@@ -50,7 +52,7 @@ public class S3Service {
 
 			return s3client.getUrl(bucket, fileName).toURI();
 		} catch (URISyntaxException e) {
-			throw new RuntimeException("Erro ao converter URL para URI");
+			throw new FileException("Erro ao converter URL para URI");
 		}
 	}
 }
