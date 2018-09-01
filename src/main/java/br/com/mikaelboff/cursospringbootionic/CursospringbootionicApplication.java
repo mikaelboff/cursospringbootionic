df69839f5af0fa4b1e38ba698132b1ae524bdb54
Mikael Boff <mikaelboff1@gmail.com>
2018-09-01 18:18:04 -0300

    Enviando imagem via endpoint

--- src/main/java/br/com/mikaelboff/cursospringbootionic/CursospringbootionicApplication.java
+++ src/main/java/br/com/mikaelboff/cursospringbootionic/CursospringbootionicApplication.java
@@ -1,17 +1,11 @@
 package br.com.mikaelboff.cursospringbootionic;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import br.com.mikaelboff.cursospringbootionic.services.S3Service;
-
 @SpringBootApplication
 public class CursospringbootionicApplication implements CommandLineRunner {
-
-	@Autowired
-	private S3Service s3Service;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursospringbootionicApplication.class, args);
@@ -19,6 +13,6 @@ public class CursospringbootionicApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... args) throws Exception {
-		s3Service.uploadFile("C:\\temp\\eu.jpg");
+
 	}
 }
